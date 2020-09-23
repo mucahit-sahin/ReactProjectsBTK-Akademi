26a70ff3e6cd5d89fbe61005c15f38e0d55d6936
mucahit-sahin <mucahitsahin786@gmail.com>
2020-09-23 14:43:59 +0300

    sepet detay sayfası yapıldı

--- intro/src/App.js
+++ intro/src/App.js
@@ -81,7 +81,13 @@ export default class App extends Component {
                     ></ProductList>
                   )}
                 ></Route>
-                <Route exact path="/cart" component={CartList}></Route>
+                <Route exact path="/cart" render={(props) => (
+                    <CartList
+                      {...props}
+                      cart={this.state.cart}
+                      removeFromCart={this.removeFromCart}
+                    ></CartList>
+                  )}></Route>
                 <Route component={NotFound}></Route>
               </Switch>
             </Col>
--- intro/src/CartList.js
+++ intro/src/CartList.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react'
+import {Table} from "reactstrap"
 
 export default class CartList extends Component {
+
+    renderCart(){
+        return(
+            <Table stripad>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Category Id</th>
+                        <th>Product Name</th>
+                        <th>unit price</th>
+                        <th>Quantity</th>
+                        <th>Units In Stock</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.props.cart.map(cartItem=>(
+                            <tr key={cartItem.product.id}>
+                                <td>{cartItem.product.id}</td>
+                                <td>{cartItem.product.categoryId}</td>
+                                <td>{cartItem.product.productName}</td>
+                                <td>{cartItem.product.unitPrice}</td>
+                                <td>{cartItem.quantity}</td>
+                                <td>{cartItem.product.unitsInStock}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </Table>
+        )
+    }
+
     render() {
         return (
             <div>
-                CartList
+                {this.renderCart()}
             </div>
         )
     }
