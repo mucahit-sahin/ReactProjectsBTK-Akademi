17a960b045bab14b29da3d6cfefa79f701bb107f
mucahit-sahin <mucahitsahin786@gmail.com>
2020-09-23 19:48:00 +0300

    DescreaseCounter, IncreaseCounter, IncreaseByTwoCounter projenin tamamlanması

--- redux-intro/src/components/IncreaseByTwoCounter.js
+++ redux-intro/src/components/IncreaseByTwoCounter.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import {increaseByTwCounter} from "../redux/actions/counterActions"
 
-export default class IncreaseByTwoCounter extends Component {
+
+class IncreaseByTwoCounter extends Component {
     render() {
         return (
             <div>
-                
+                <button onClick={e=>{
+                    this.props.dispatch(increaseByTwCounter())
+                }}>
+                    2 arttır
+                </button>
             </div>
         )
     }
 }
+
+function mapDispatchToProps(dispatch){
+    return {actions:bindActionCreators(increaseByTwCounter,dispatch)}
+}
+
+export default connect(mapDispatchToProps)(IncreaseByTwoCounter);
