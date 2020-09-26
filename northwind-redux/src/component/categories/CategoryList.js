abfaf8e03bc1df5faf9e8ad50f266a0534c8c477
mucahit-sahin <mucahitsahin786@gmail.com>
2020-09-26 15:49:20 +0300

    redux thunk ile çalışmak

--- northwind-redux/src/component/categories/CategoryList.js
+++ northwind-redux/src/component/categories/CategoryList.js
@@ -1,20 +1,50 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as categoryActions from "../../redux/actions/categoryActions";
+import { ListGroup, ListGroupItem } from "reactstrap";
 
 class CategoryList extends Component {
+  componentDidMount() {
+    this.props.actions.getCategories();
+  }
+
   render() {
     return (
       <div>
-        <h3>categorylist</h3>
+        <h3>Categories {this.props.categories.length}</h3>
+        <ListGroup>
+          {this.props.categories.map((category) => (
+            <ListGroupItem onClick={()=>this.props.actions.changeCategory(category)} key={category.id}>
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
+        </ListGroup>
         <h5>Seçili Kategori : {this.props.currentCategory.categoryName}</h5>
       </div>
     );
   }
 }
+
 function mapStateToProps(state) {
   return {
     currentCategory: state.changeCategoryReducer,
+    categories: state.categoryListReducer,
   };
 }
 
-export default connect(mapStateToProps)(CategoryList);
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      getCategories: bindActionCreators(
+        categoryActions.getCategories,
+        dispatch
+      ),
+      changeCategory:bindActionCreators(
+        categoryActions.changeCategory,
+        dispatch
+      )
+    },
+  };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryList);
